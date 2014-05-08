7fe07dc732db76b786cab96e217f865113e5a1aa
shakdaniel <web.designs@icloud.com>
2014-05-08 02:11:13 -0400

    package.json updated with dev dependencies and gulpfile.js updated with requires and comments.

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,42 @@
-var gulp
+/****************************
+GULP DEPENDENCIES & PLUGINS
+*****************************/
+// gulp: 			node task runner
+// clean:  		 	removes files/folders
+// jade:   		 	compile jade into html files
+// stylus: 		 	compile stly into css files
+// autoprefixer: 	sets missing browser prefixes
+// minifycss: 	 	minify css files
+// jshint: 		 	debug js files
+// stylish: 		make jshint style pretty
+// uglify: 		 	minify js files
+// concat: 		 	merge all js files into one file
+// rename: 		 	chanage file names
+// filesize: 	 	chanage file names
+// changes: 	 	pass only changed files
+// imagemin: 	 	minify png jpeg gif svg files
+// notify: 		 	send notification messages
+// plumber: 		disable plugin interuptions
+// watch: 			watch files in an endless stream
+// livereload: 		used for livereload
+// browsersync: 	live css reload & browser syncing
+
+var gulp = require('gulp'),
+    clean = require('gulp-clean'),
+    jade = require('gulp-jade'),
+    stylus = require('gulp-stylus'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minifycss = require('gulp-minify-css'),
+    jshint = require('gulp-jshint'),
+    stylish = require('jshint-stylish'),
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat'),
+    rename = require('gulp-rename'),
+    filesize = require('gulp-filesize'),
+    changed = require('gulp-changed'),
+    imagemin = require('gulp-imagemin'),
+    notify = require('gulp-notify'),
+    plumber = require('gulp-plumber'),
+    watch = require('gulp-watch'),
+    livereload = require('gulp-livereload'),
+    browserSync = require('browser-sync'),;
